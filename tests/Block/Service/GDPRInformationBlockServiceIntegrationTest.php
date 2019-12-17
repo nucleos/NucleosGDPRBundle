33384164feb3d812948744092dc2c9fe33f16861
core23 <mail@core23.de>
2019-12-17 17:28:22 +0100

    Add integration test

---
+++ tests/Block/Service/GDPRInformationBlockServiceIntegrationTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * (c) Christian Gripp <mail@core23.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Core23\GDPRBundle\Tests\Block\Service;
+
+use Core23\GDPRBundle\Tests\App\AppKernel;
+use PHPUnit\Framework\TestCase;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Component\HttpKernel\Client;
+
+final class GDPRInformationBlockServiceIntegrationTest extends TestCase
+{
+    public function testRender(): void
+    {
+        if (class_exists(KernelBrowser::class)) {
+            $client = new KernelBrowser(new AppKernel());
+        } else {
+            $client = new Client(new AppKernel());
+        }
+
+        $client->request('GET', '/twig-test');
+
+        static::assertSame(200, $client->getResponse()->getStatusCode());
+    }
+}
