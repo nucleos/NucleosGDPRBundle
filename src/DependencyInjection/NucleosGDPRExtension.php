6c81db8ae3ab1ca03669f213986aad35ff5b0047
core23 <mail@core23.de>
2024-08-14 17:43:14 +0200

    Fix symfony deprecation

--- src/DependencyInjection/NucleosGDPRExtension.php
+++ src/DependencyInjection/NucleosGDPRExtension.php
@@ -16,8 +16,8 @@ namespace Nucleos\GDPRBundle\DependencyInjection;
 use Nucleos\GDPRBundle\EventListener\KernelEventSubscriber;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 final class NucleosGDPRExtension extends Extension
 {
